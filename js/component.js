8dd2ba3a5a1b0c3cfafd50299c8013924d59798b
Ondřej Maxa <ondrej.maxa11@gmail.com>
2023-01-11 00:11:38 +0100

    :sparkles: web components in js

---
+++ js/component.js
@@ -0,0 +1,48 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+/**
+ * @param {HTMLCustomElement} element
+ */
+function parseProps(element) {
+  const props = {};
+  element.getAttributeNames().forEach(n => {
+    props[n] = element.getAttribute(n);
+  });
+  return props;
+}
+
+class HTMLCustomElement extends HTMLElement {
+  /**
+   * Constructor
+   * @param {string} name name of the custom element
+   * @param {*} template html template
+  */
+  constructor(name, template) {
+    super();
+    this.attachShadow({ mode: "open" });
+    const props = parseProps(this);
+    render(
+      html`
+      <style>@import "/assets/css/${name}.min.css";</style>
+      ${template(props)}
+      `,
+      this.shadowRoot);
+  }
+}
+
+async function loadComponent([name, component]) {
+  window.customElements.define(name, class extends HTMLCustomElement {
+      constructor() {
+        super(name, component.default);
+        (component.listeners ?? [])
+          .forEach(([ event, query, callback ]) => {
+            this.shadowRoot.querySelector(query).addEventListener(event, callback);
+          });
+      }
+    });
+}
+
+Object.entries(window.components)
+  .map(loadComponent)
+  .forEach(p => p.catch(console.error));
+
